7eddc75ea4fa88e4611e530226655d2d2e09534c
Kamil Kałuża <kamil321.94@gmail.com>
2022-12-14 13:57:35 +0100

    wired up pagination buttons

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log(" IM A LIVE ")
+const multiStepForm = document.querySelector("[data-multi-step]")
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
+let currentStep = formSteps.findIndex(step => {
+  return step.classList.contains("active")
+})
+
+if (currentStep < 0) {
+  currentStep = 0
+  showCurrentStep()
+  hoverRest()
+
+}
+
+multiStepForm.addEventListener("click", e => {
+  let incrementor
+  if (e.target.matches("[data-next]")) {
+    incrementor = 1
+  } else if (e.target.matches("[data-previous]")) {
+    incrementor = -1
+  }
+
+  if (incrementor == null) return
+
+  const inputs = [...formSteps[currentStep].querySelectorAll("input")]
+  const allValid = inputs.every(input => input.reportValidity())
+  if (allValid) {
+    currentStep += incrementor;
+    showCurrentStep();
+    hoverRest();
+  }
+})
+
+
+
+function showCurrentStep() {
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("on", index === currentStep)
+  })
+}
+
+
+function hoverRest() {
+    formSteps.forEach((step, index) => {
+      step.classList.toggle("off", index !== currentStep)
+    })
+  }
